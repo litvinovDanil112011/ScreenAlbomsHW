4fe45a8548c25e75a71720488c39dc862cbe6974
litvinovDanil112011 <danil112011@mail.ru>
2022-06-10 17:09:59 +0300

    подготовил ячейки и модель.

--- ScreenAlbomsHW/Controllers/AlbomsViewController.swift
+++ ScreenAlbomsHW/Controllers/AlbomsViewController.swift
@@ -7,10 +7,31 @@
 
 import UIKit
 
-class AlbomsViewController: UIViewController {
+class AlbomsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    
+    private let collectionsView: UICollectionView = {
+        let collectionsView = UICollectionView()
+        collectionsView.register(PhotoCells.self, forCellWithReuseIdentifier: PhotoCells.idintifear)
+        return collectionsView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Альбомы"
         view.backgroundColor = .white
+        collectionsView.delegate = self
+        collectionsView.dataSource = self
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCells.idintifear, for: indexPath)
+        return cell
+    }
+    
 }
